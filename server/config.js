2a030deae0b4f3923e64db01c95cd83eabf8eb5d
ubuntu <ubuntu@ubuntu>
2017-09-29 13:02:40 -0700

    change database to mongo atlas

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'secret',
-    'database': 'mongodb://localhost/learnang2'
+    'database': 'mongodb://overmellow:eYe3bN1MeoMUiPJ4@cluster0-shard-00-00-uo49g.mongodb.net:27017,cluster0-shard-00-01-uo49g.mongodb.net:27017,cluster0-shard-00-02-uo49g.mongodb.net:27017/learnang2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+//    'database': 'mongodb://localhost/learnang2',
 };
